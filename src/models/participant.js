18d9ca13a6f05a732f944d8f9fd0a4b2740518bc
Ksenia <ksenia.ivanovva@gmail.com>
2023-03-14 19:59:16 +0100

    feat: added participant schema

---
+++ src/models/participant.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const participantSchema = new mongoose.Schema({
+
+namn: {
+    type: String,
+    required: true
+}, 
+
+email: {
+    type: String,
+    required: true
+}, 
+
+role: {
+ type: String,   
+ enum: ["ledare", "följare"],
+ required: true
+},
+
+betalningsstatus: {
+ type: String
+},
+
+danceClass: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'danceClass'
+  }
+});
+
+module.exports = mongoose.model('Participant', participantSchema);
+
